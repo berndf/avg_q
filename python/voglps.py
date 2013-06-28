95f9e5d51fcc3998218cc46eb0378d57d4d6b63b
Bernd Feige <Bernd Feige Bernd.Feige@gmx.net>
2013-06-28 12:49:41 +0200

    Added python/voglps2svg and voglps2svg.cairo to generate SVG vector files from posplot .ps plot output

---
+++ python/voglps.py
@@ -0,0 +1,60 @@
+# Copyright (C) 2008-2012 Bernd Feige
+# This file is part of avg_q and released under the GPL v3 (see avg_q/COPYING).
+
+# Base class for parsing the very simple VOGL .ps output
+
+scale_factor=1.9 # Divide all coordinates by this to avoid overflowing oodraw's coordinates
+
+class voglps(object):
+ def __init__(self,psfile):
+  self.infile=open(psfile,"r")
+  #self.color=(1,0,0)
+  self.linestarted=False
+ def _stroke(self):
+  if self.linestarted:
+   self.stroke()
+   self.linestarted=False
+ def _moveto(self,x,y):
+  self._stroke()
+  self.moveto(float(x),float(y))
+ def _lineto(self,x,y):
+  self.lineto(x,y)
+  self.linestarted=True
+ def _text(self,text):
+  if text.strip()!='':
+   self.text(text)
+ def _color(self,r,g,b):
+  # Translate white to black:
+  if r==1 and g==1 and b==1:
+   r,g,b=0,0,0
+  self.color(r,g,b)
+ # vvv Derived class interface vvv
+ def stroke(self):
+  print("Stroke")
+ def moveto(self,x,y):
+  print("%g %g" % (x,y))
+ def lineto(self,x,y):
+  print("-> %g %g" % (x,y))
+ def text(self,text):
+  print(text)
+ def color(self,r,g,b):
+  print("Color %g %g %g" % (r,g,b))
+ # ^^^ Derived class interface ^^^
+ def parse(self):
+  for line in self.infile:
+   line=line.rstrip('\r\n')
+   if line.endswith(" m"):
+    x,y,o=line.split()
+    self._moveto(float(x)/scale_factor,float(y)/scale_factor)
+   elif line.endswith(" d"):
+    x,y,o=line.split()
+    self._lineto(float(x)/scale_factor,float(y)/scale_factor)
+   elif line.endswith(" s"):
+    text=line[1:-3]
+    self._text(text)
+   elif line.endswith(" c"):
+    r,g,b,o=line.split()
+    self._color(float(r),float(g),float(b))
+ def close(self):
+  self._stroke()
+  self.infile.close()
---
+++ python/voglps2svg.cairo
@@ -0,0 +1,44 @@
+#!/bin/env python
+# Copyright (C) 2008-2012 Bernd Feige
+# This file is part of avg_q and released under the GPL v3 (see avg_q/COPYING).
+
+import sys
+import voglps
+import cairo
+
+# Derived class with cairo plotting operators
+class cairovogl(voglps.voglps):
+ def __init__(self,psfile,surface,width,height):
+  voglps.voglps.__init__(self,psfile)
+  self.ctx = cairo.Context(surface)
+  self.ctx.set_font_size(20)
+  self.width=width
+  self.height=height
+ def moveto(self,x,y):
+  self.ctx.move_to(x,self.height-y)
+ def lineto(self,x,y):
+  self.ctx.line_to(x,self.height-y)
+ def stroke(self):
+  self.ctx.stroke()
+ def text(self,text):
+  self.ctx.show_text(text)
+ def color(self,r,g,b):
+  self._stroke()
+  self.ctx.set_source_rgb(r,g,b)
+
+# These are the postscript coordinates
+WIDTH, HEIGHT  = 2700/voglps.scale_factor, 2000/voglps.scale_factor
+
+for psfile in sys.argv[1:]:
+ svgfile=psfile.replace(".ps",".svg")
+ print(svgfile)
+ surface = cairo.SVGSurface(svgfile, WIDTH, HEIGHT)
+ #v=voglps.voglps(psfile)
+ v=cairovogl(psfile,surface,WIDTH,HEIGHT)
+ #ctx.scale (WIDTH/1.0, HEIGHT/1.0)
+ v.parse()
+ #ctx.rel_line_to(1000,1000)
+ #ctx.line_to(0,1000)
+ #ctx.set_source_rgb(0,1,0)
+ #ctx.stroke()
+ surface.finish()
