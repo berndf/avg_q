d7d07d4326671292b7d8826b73ff1e70c8c1e3eb
Bernd Feige <Bernd Feige Bernd.Feige@gmx.net>
2018-02-27 15:38:37 +0100

    Merged avg_q_ui/avg_q_assoc.bat and its Windows install rule.

---
+++ avg_q_ui/avg_q_assoc.bat
@@ -0,0 +1,17 @@
+REM Associate show_any with (most of) the known file types.
+
+REM ftype avg_q.File="C:\Python36\python.exe" Q:\PPT-A-EKP-Labor\avg_q32\python\show_any "%1" %*
+ftype avg_q.File="C:\Python36\python.exe" C:\avg_q64\python\show_any "%1" %*
+assoc .avg=avg_q.File
+assoc .eeg=avg_q.File
+assoc .cnt=avg_q.File
+assoc .vhdr=avg_q.File
+assoc .asc=avg_q.File
+assoc .hdf=avg_q.File
+assoc .edf=avg_q.File
+assoc .rec=avg_q.File
+assoc .bdf=avg_q.File
+assoc .emg=avg_q.File
+assoc .vpd=avg_q.File
+assoc .ebm=avg_q.File
+assoc .cfs=avg_q.File
